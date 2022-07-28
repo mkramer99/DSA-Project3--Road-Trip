721017618852232bdc533a0b4d58361129ed8f93
Melanie Kramer <92834107+mkramer99@users.noreply.github.com>
2022-07-28 17:34:22 -0400

    Update Algorithms.h
    Uploaded Bellman fix

--- DSA-Project3/Algorithms.h
+++ DSA-Project3/Algorithms.h
@@ -11,7 +11,17 @@
 #include <limits>
 #include <set>
 #include <stack>
+#include <map>
+#include <vector>
+#include <queue>
+#include <chrono>
+using namespace std::chrono;
+using Clock = std::chrono::steady_clock;
 
+// there are two Dijstra implementations
+// the second one is utilized in main method
+// first will be commented out
+/*
 stack<int> Dijkstra(map<int, vector<pair<int, float>>> graph, int source, int dest) {
 
     int V = graph.size(); // Represents the number of vertices.
@@ -84,62 +94,56 @@ stack<int> Dijkstra(map<int, vector<pair<int, float>>> graph, int source, int de
     path.push(source); // Pushes source to the top of the stack to complete the path.
 
     return path;
-
 }
+*/
 
-stack<int> BellmanFord(map<int, vector<pair<int, float>>> graph, int source, int dest, int E) {
-
+float BellmanFord(map<int, vector<pair<int, float>>> graph, int source, int dest) {
+    cout << "Bellman Ford" << endl;
+    auto tic = Clock::now();
     int V = graph.size(); // Represents number of vertices.
-    int d[V]; // Holds distances from the source vertex to each vertex in the array.
+    float d[V]; // Holds distances from the source vertex to each vertex in the array.
     int p[V]; // Contains predecessor node indices, for finding the shortest path.
-
+    set<int> S; // Computed vertices.
+    float distance = 0;
+    int nodes = 0;
+    cout << "first for loop" << endl;
     for (int i = 0; i < V; i++) {
-
         d[i] = numeric_limits<int>::max(); // Sets each value in 'd' to 'infinity' (max integer value).
         p[i] = -1;
-
     }
 
     d[source] = 0;
-
-    for (int i = 0; i < V - 1; i++) { // Relaxes all edges in the graph |V - 1| times.
-
-        for (auto pair : graph) { // Iterates through each entry in the Adjacency List.
-
-            for (auto subPair : pair.second) { // Iterates through each entry in the corresponding vector.
-
-                if (d[pair.first] != numeric_limits<int>::max() && d[subPair.first] > (d[pair.first] + subPair.second) {
-
-                    d[subPair.first] = d[pair.first] + subPair.second;
-                    p[subPair.first] = pair.first;
-
+    cout << "nested for loop" << endl;
+    bool flag = true;
+    while (flag == true) {
+        for (int i = 0; i < V - 1; i++) { // Relaxes all edges in the graph |V - 1| times.
+            for (auto pair: graph) { // Iterates through each entry in the Adjacency List.
+                for (auto subPair: pair.second) { // Iterates through each entry in the corresponding vector.
+                    if (d[pair.first] != numeric_limits<int>::max() &&
+                        d[subPair.first] > (d[pair.first] + subPair.second)) {
+                        d[subPair.first] = d[pair.first] + subPair.second;
+                        p[subPair.first] = pair.first;
+                        nodes++;
+                        if (pair.first == dest) {
+                            cout << "DESTINATION BELLMAN " << pair.first << endl;
+                            cout << d[subPair.first] << " " << d[pair.first] << endl;
+                            distance = d[subPair.first];
+                            flag = false;
+                            break;
+                        }
+                    }
                 }
-
+                if (flag == false) { break; }
             }
-
+            if (flag == false) { break; }
         }
-
     }
-
-    stack<int> path; // Represents the shortest path found; source on the top, destination on the bottom.
-    path.push(dest); // Thus, destination is pushed first.
-
-    int temp = p[dest];
-
-    while (temp != source) {
-
-        path.push(temp);
-        temp = p[temp];
-
-    }
-
-    path.push(source); // Pushes source to the top of the stack to complete the path.
-
-    return path;
-
+    auto toc = Clock::now();
+    cout << "elapsed time: " << duration_cast<std::chrono::milliseconds>(toc - tic).count() << endl;
+    return distance;
 }
                     
-                    
+// MKramer                  
 float Dijkstra(map<int, vector<pair<int, float>>> graph, int source, int dest) {
     cout << "entered Dijkstra" << endl;
     auto tic = Clock::now();
