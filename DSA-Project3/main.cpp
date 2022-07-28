47e45fdb658099a49f307a0985017ee9a41c1524
Melanie Kramer <92834107+mkramer99@users.noreply.github.com>
2022-07-28 14:56:06 -0400

    Update main.cpp

--- DSA-Project3/main.cpp
+++ DSA-Project3/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "Graph.h"
+#include "Algorithms.h"
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // load the road network graph
+    Graph g;
+    // to test Dijkstra
+    stack<int> path = Dijkstra(g.graph, 147477, 166505);
+    int d = 0;
+    while (!path.empty()) {
+        d += path.top();
+        path.pop();
+    }
+    cout << "distance in main: " << d << endl;
+    //float path = Dijkstra(g.graph, 147477, 166504);
+    //cout << path;
     return 0;
 }
