d34e8393cc0895d84f843f5e12ec88d111539118
Melanie Kramer <92834107+mkramer99@users.noreply.github.com>
2022-07-28 15:10:59 -0400

    Update Algorithms.h
    Update Dijkstra with priority queue

--- DSA-Project3/Algorithms.h
+++ DSA-Project3/Algorithms.h
@@ -138,3 +138,72 @@ stack<int> BellmanFord(map<int, vector<pair<int, float>>> graph, int source, int
     return path;
 
 }
+                    
+                    
+float Dijkstra(map<int, vector<pair<int, float>>> graph, int source, int dest) {
+    cout << "entered Dijkstra" << endl;
+    auto tic = Clock::now();
+    int V = graph.size(); // Represents the number of vertices.
+    set<int> S; // Computed vertices.
+    set<int> VS; // Vertices to be computed.
+    priority_queue<pair<int, float>> pq;
+
+    for (auto pair: graph) {
+
+        VS.emplace(pair.first);
+    }
+    cout << "VS initial size: " << VS.size() << endl;
+    float d[V]; // Contains distances.
+    int p[V]; // Contains predecessor node indices, for finding the shortest path.
+
+    for (int i = 0; i < V; i++) {
+
+        d[i] = numeric_limits<int>::max(); // Sets each value in 'd' to 'infinity' (max integer value).
+        p[i] = -1;
+
+    }
+    cout << "numeric for loop complete" << endl;
+    d[source] = 0;
+    float distance = 0;
+    int nodes = 0;
+    int min = source; // 'min' tracks the closest node to source in 'VS', initially source itself.
+    pq.push(make_pair(source, 0));
+    while (!pq.empty()) {
+
+
+        pair<int, float> curr = pq.top();
+        pq.pop();
+        int u = curr.first;
+        for (auto it = graph[u].begin(); it != graph[u].end(); it++) {
+            int v = it->first; // neighbor
+            float w = it->second; // weight
+            // if node not already checked
+            if (S.find(v) == S.end()) {
+                if (d[v] > d[u] + w) {
+                    d[v] = d[u] + w;
+                    pq.push(make_pair(v, d[v]));
+
+                    S.emplace(v);
+                    VS.erase(V);
+                    nodes++;
+                    if (v == dest) {
+                        cout << "DESTINATION" << endl;
+                        cout << pq.size() << endl;
+                        distance = d[v];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    stack<int> path; // Represents the shortest path found; source on the top, destination on the bottom.
+    path.push(dest); // Thus, destination is pushed first.
+    cout << "distance: " << distance << endl;
+    cout << "nodes checked: " << nodes << endl;
+
+    auto toc = Clock::now();
+    cout << "elapsed time: " << duration_cast<std::chrono::milliseconds>(toc - tic).count() << endl;
+    return distance;
+}
+                    
